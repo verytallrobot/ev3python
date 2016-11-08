08194713082bff65addeed59750bb481d0b932d5
verytallrobot <verytallrob@gmail.com>
2016-11-09 02:23:36 +0800

    changed stop action after constructing object

--- m3.py
+++ m3.py
@@ -7,8 +7,8 @@ from ev3dev.ev3 import *
 from time import sleep
 
 
-m = LargeMotor('outB', stop_action="hold")
-
+m = LargeMotor('outB')
+m.stop_action="hold"
 
 m.run_to_rel_pos(position_sp=360, speed_sp=900)
 
